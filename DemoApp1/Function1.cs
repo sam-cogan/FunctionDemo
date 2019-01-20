398e9afd31be3725eed37dfa89fcfb16e61019dc
Sam Cogan <mail@samcogan.com>
2019-01-20 16:55:00 +0000

    Add Project

---
+++ DemoApp1/Function1.cs
@@ -0,0 +1,34 @@
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace DemoApp1
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req, [CosmosDB(
+                databaseName: "FunctionLogDemo",
+                collectionName: "DemoCollection",
+                ConnectionStringSetting = "CosmosDBConnection")]out dynamic document,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            var logEvent =JsonConvert.DeserializeObject<LogData>(requestBody as string);
+
+            document = new { Id = Guid.NewGuid(), Message= logEvent.EventMessage, Time=logEvent.EventTime, Category=logEvent.EventCategory };
+
+        }
+    }
+}
---
+++ DemoApp1/LogData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoApp1
+{
+    class LogData
+    {
+        public string EventMessage { get; set; }
+        public string EventCategory { get; set; }
+
+        public DateTime EventTime { get; set; }
+    }
+}
